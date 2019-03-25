3d1b337239da961b4a6f0d4b3016d7a285575db0
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2019-03-25 20:56:24 +0000

    consolidating search js; updating a couple of packages

--- js/search-settings.js
+++
@@ -1,7 +0,0 @@
-SimpleJekyllSearch({
-    searchInput: document.getElementById('search-input'),
-    resultsContainer: document.getElementById('results-container'),
-    json: '/search.json',
-    searchResultTemplate: '<a href="{relative_url}"><li class="list-group-item">{title}</li></a>',
-    noResultsText: `<p>No results found.</p>`
-})
